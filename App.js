e231c4910adb5c79e20dc16dc1fcbf808fd6340b
elbarea <rafaelbh15@hotmail.es>
2021-10-14 13:38:39 +0200

    flexDirection tests Tests with flexDirection values: row, reverse-row, column (default mode) and reverse-column.

--- App.js
+++ App.js
@@ -22,23 +22,26 @@ export default function App() {
   console.log(StatusBar.currentHeight);
   const { landscape } = useDeviceOrientation();
   return (
-    <View style={{backgroundColor: "#ffff", flex: 1}}>
+    <View style={{ backgroundColor: "#ffff", flex: 1, flexDirection: "row" }}>
       <View
         style={{
           backgroundColor: "dodgerblue",
-          flex: 2
+          width: 100,
+          height: 100,
         }}
       ></View>
       <View
         style={{
           backgroundColor: "gold",
-          flex: 1
+          width: 100,
+          height: 100,
         }}
       ></View>
       <View
         style={{
           backgroundColor: "tomato",
-          flex: 1
+          width: 100,
+          height: 100,
         }}
       ></View>
 
