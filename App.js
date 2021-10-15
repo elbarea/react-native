75f5260282ee2b3f4fb394f309e84162a9bd3eb6
elbarea <rafaelbh15@hotmail.es>
2021-10-15 09:25:21 +0200

    ViewImageScreen Created ViewImageScreen

--- App.js
+++ App.js
@@ -1,40 +1,7 @@
 import React from "react";
-import {
-  StyleSheet,
-  View,
-  Text,
-  StatusBar,
-  ImageBackground,
-  Image,
-} from "react-native";
 import ViewImageScreen from "./screens/ViewImageScreen";
 
 import WelcomeScreen from "./screens/WelcomeScreen";
 export default function App() {
-  return <WelcomeScreen />
-  
+  return <ViewImageScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  button: {
-    flex: 1,
-    width: "100%",
-  },
-  backWithLogo: {
-    flex: 8,
-    width: "100%",
-    height: "100%",
-    alignItems: "center",
-  },
-  logo: {
-    width: 100,
-    height: 100,
-    marginTop: 50
-  },
-});
---
+++ screens/ViewImageScreen.js
@@ -0,0 +1,50 @@
+import React from "react";
+import {
+  Image,
+  ImageBackground,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+
+export default function ViewImageScreen() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.closeIcon}></View>
+      <View style={styles.deleteIcon}></View>
+      <Image
+        source={require("../assets/chair.jpg")}
+        style={styles.img}
+        resizeMode="contain"
+      ></Image>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#000",
+  },
+  closeIcon: {
+    height: 50,
+    width: 50,
+    position: "absolute",
+    top: 20,
+    left: 30,
+    backgroundColor: "#fc5c65",
+  },
+  deleteIcon: {
+    height: 50,
+    width: 50,
+    position: "absolute",
+    top: 20,
+    right: 30,
+    backgroundColor: "#4ECDC4"
+  },
+  img: {
+    width: "100%",
+    height: "100%",
+  },
+});
